56a8ead5468204de0210cf7607646274b3c9aaaf
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-15 16:37:30 +0200

    use new api

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,6 +1,6 @@
 export default function (context) {
   return {
-    httpEndpoint: 'http://128.199.41.13:4466',
-    getAuth: () => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2NjEwMTEzLCJleHAiOjE1ODcyMTQ5MTN9.9HiseWjTpFm0ySYt0T2OrmPgG70oJ65cslLLKT9fqoA'
+    httpEndpoint: 'https://atfrg.online/api/',
+     getAuth: () => ""
   }
 }
