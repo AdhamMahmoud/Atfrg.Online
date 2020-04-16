92da2ff87b7a7f0f248b1607fc20ef1645505965
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-16 14:59:20 +0200

    ll

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,12 +63,12 @@ export default {
         async :"",
         type: "text/javascript"
       },
-      {
-        src: "//inpagepush.com/400/3218692",
-        async :"async",
-        "data-cfasync" :"false",
-        type: "text/javascript"
-      },
+      // {
+      //   src: "//inpagepush.com/400/3218692",
+      //   async :"async",
+      //   "data-cfasync" :"false",
+      //   type: "text/javascript"
+      // },
     ]
   },
   /*
