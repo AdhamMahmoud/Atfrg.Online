4d741764a2d6e3b253abe64782e5fce4e8b17efb
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-27 01:56:32 +0200

    new

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,12 +1,12 @@
-export default function (context) {
-  return {
-    httpEndpoint: 'https://atfrg.online/api/',
-     getAuth: () => ""
-  }
-}
 // export default function (context) {
 //   return {
-//     httpEndpoint: 'https://atfrg.space/database/',
-//      getAuth: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU"
+//     httpEndpoint: 'https://atfrg.online/api/',
+//      getAuth: () => ""
 //   }
 // }
+export default function (context) {
+  return {
+    httpEndpoint: 'https://atfrg.space/database/',
+     getAuth: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU"
+  }
+}
