23ded7a0a5e7625dbe88b9790ba8fc9b24fb88d9
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-28 22:09:53 +0200

    aa

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -7,11 +7,11 @@
 export default function (context) {
   return {
     // ssr:true,
-    httpEndpoint: 'https://atfrg.space/database/',
+    httpEndpoint: '//atfrg.space/database/',
     httpLinkOptions: {
       credentials: 'same-origin'
     },
-    wsEndpoint: 'wss://atfrg.space/database/',
+    wsEndpoint: 'ws://atfrg.space/database/',
     websocketsOnly: true,
      getAuth: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU",
    
