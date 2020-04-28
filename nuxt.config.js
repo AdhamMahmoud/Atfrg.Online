429f363ff2ee81daee2d536de21b7420ae250bb1
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-28 22:17:14 +0200

    aas

--- nuxt.config.js
+++ nuxt.config.js
@@ -108,6 +108,19 @@ export default {
   ],
   apollo: {
     includeNodeModules: false,
+    cookieAttributes: {
+      /**
+        * Define when the cookie will be removed. Value can be a Number
+        * which will be interpreted as days from time of creation or a
+        * Date instance. If omitted, the cookie becomes a session cookie.
+        */
+      expires: 7, // optional, default: 7 (days)
+      /**
+        * A Boolean indicating if the cookie transmission requires a
+        * secure protocol (https). Defaults to false.
+        */
+      secure: true,
+    },
     clientConfigs: {
       default: "~/plugins/my-alternative-apollo-config.js"
       // alternative: user path to config which returns exact same config options
--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -12,7 +12,7 @@ export default function (context) {
       credentials: 'same-origin'
     },
     wsEndpoint: 'ws://atfrg.space/database/',
-    websocketsOnly: true,
+    websocketsOnly: false,
      getAuth: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU",
    
   }
