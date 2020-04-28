38f2f07a8ddc1043290d5bb32f980682465ddb6b
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-28 21:14:21 +0200

    aaa

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -7,7 +7,7 @@
 export default function (context) {
   return {
     // ssr:true,
-    httpEndpoint: 'https://atfrg.space/database/',
+    httpEndpoint: 'atfrg.space/database/',
      getAuth: () => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU"
   }
 }
