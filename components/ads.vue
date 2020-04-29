9387a133b92aefa72fdf02172d349e82f932e33c
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-29 16:09:42 +0200

    a2

--- components/ads.vue
+++ components/ads.vue
@@ -1,33 +1,33 @@
 <template>
 <div class="container">
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-12">
             <div id="ads" class="ads">
                 <p> الأعلانات دي عشانك عشان نستمر ❤️</p>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 </template>
-<script>
-export default {
-       data: function () {
-        return {
-            ads:null,
-        }
-       },
-       mounted(){
-        var list = document.getElementById("ads");
-        this.ads = document.createElement("div");
-        var sc = document.createElement('script');
-        // this.ads.classList.add("vide-ad");
-        sc.setAttribute('data-cfasync','false');
-        sc.setAttribute('src','//native.propellerclick.com/1?z=3252004');
-        this.ads.appendChild(sc);
-        list.appendChild(this.ads);
-       }
-}
-</script>
+// <script>
+// export default {
+//        data: function () {
+//         return {
+//             ads:null,
+//         }
+//        },
+//        mounted(){
+//         var list = document.getElementById("ads");
+//         this.ads = document.createElement("div");
+//         var sc = document.createElement('script');
+//         // this.ads.classList.add("vide-ad");
+//         sc.setAttribute('data-cfasync','false');
+//         sc.setAttribute('src','//native.propellerclick.com/1?z=3252004');
+//         this.ads.appendChild(sc);
+//         list.appendChild(this.ads);
+//        }
+// }
+// </script>
 <style lang="scss">
 .ads{
     text-align: center;
--- components/ads2.vue
+++ components/ads2.vue
@@ -1,32 +1,32 @@
 <template>
 <div class="container">
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-12">
             <div id="ads-download" class="ads">
                 <p> الأعلانات دي عشانك عشان نستمر ❤️</p>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 </template>
 <script>
-export default {
-       data: function () {
-        return {
-            ads:null,
-        }
-       },
-       mounted(){
-        var list = document.getElementById("ads-download");
-        this.ads = document.createElement("div");
-        var sc = document.createElement('script');
-        // this.ads.classList.add("vide-ad");
-        sc.setAttribute('data-cfasync','false');
-        sc.setAttribute('src','//p393613.clksite.com/adServe/banners?tid=393613_773071_5');
-        this.ads.appendChild(sc);
-        list.appendChild(this.ads);
-       }
-}
+// export default {
+//        data: function () {
+//         return {
+//             ads:null,
+//         }
+//        },
+//        mounted(){
+//         var list = document.getElementById("ads-download");
+//         this.ads = document.createElement("div");
+//         var sc = document.createElement('script');
+//         // this.ads.classList.add("vide-ad");
+//         sc.setAttribute('data-cfasync','false');
+//         sc.setAttribute('src','//p393613.clksite.com/adServe/banners?tid=393613_773071_5');
+//         this.ads.appendChild(sc);
+//         list.appendChild(this.ads);
+//        }
+// }
 </script>
 <style lang="scss">
 .ads{
