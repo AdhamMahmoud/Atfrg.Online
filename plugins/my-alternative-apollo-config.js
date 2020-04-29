21013c99424be281d17f321f0bf6fde46fb2d96a
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-29 08:41:22 +0200

    aa

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,7 +1,8 @@
 
 export default function(context){
   return {
-     httpEndpoint: 'https://atfrg.space/database/',
+     httpEndpoint: 'https://atfrg.online/database/',
+     ssr:true,
     // optional
     // override HTTP endpoint in browser only
     // browserHttpEndpoint: '/graphql',
@@ -12,7 +13,7 @@ export default function(context){
     },
     // You can use `wss` for secure connection (recommended in production)
     // Use `null` to disable subscriptions
-     wsEndpoint: 'wss://atfrg.space/database/', // optional
+     wssEndpoint: 'wss://atfrg.online/database/', // optional
     // LocalStorage token
     tokenName: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU', // optional
     // Enable Automatic Query persisting with Apollo Engine
