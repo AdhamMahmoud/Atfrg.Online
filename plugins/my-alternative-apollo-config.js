f430aa1a647bb9110ae852a37e5fb97a92764fc9
Shady Khalifa <shekohex@gmail.com>
2020-04-29 21:22:59 +0200

    use temp token

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,6 +1,6 @@
 export default function(context) {
   return {
-    httpEndpoint: "https://api.atfrg.online/",
+    httpEndpoint: "https://atfrg.space/database/",
     ssr: true,
     // optional
     // override HTTP endpoint in browser only
@@ -15,12 +15,13 @@ export default function(context) {
     //  wsEndpoint: 'wss://atfrg.space/database/',
     //  wssEndpoint: 'wss://atfrg.space/database/', // optional
     // LocalStorage token
-    // tokenName: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU', // optional
+    tokenName: "token", // optional
     // Enable Automatic Query persisting with Apollo Engine
     persisting: false, // Optional
     // Use websockets for everything (no HTTP)
     // You need to pass a `wsEndpoint` for this to work
     websocketsOnly: false, // Optional,
-    getAuth: tokenName => ""
+    getAuth: tokenName =>
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU"
   };
 }
