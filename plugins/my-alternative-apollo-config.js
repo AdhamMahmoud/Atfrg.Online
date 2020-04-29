b8609a75a4f86b52f454ab8bb530489235ae1ad4
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-29 07:59:44 +0200

    a

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,7 +1,7 @@
 
 export default function(context){
   return {
-     httpEndpoint: 'https://atfrg.space/server/',
+     httpEndpoint: 'https://atfrg.online/api2/',
     // optional
     // override HTTP endpoint in browser only
     // browserHttpEndpoint: '/graphql',
@@ -16,7 +16,7 @@ export default function(context){
     // LocalStorage token
     tokenName: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU', // optional
     // Enable Automatic Query persisting with Apollo Engine
-    persisting: false, // Optional
+    persisting: true, // Optional
     // Use websockets for everything (no HTTP)
     // You need to pass a `wsEndpoint` for this to work
     // websocketsOnly: true, // Optional,
