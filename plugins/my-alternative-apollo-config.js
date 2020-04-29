757d1459d28c66e84c033d796d8d2d0a97992358
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-04-29 08:08:54 +0200

    as

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,7 +1,7 @@
 
 export default function(context){
   return {
-     httpEndpoint: 'https://atfrg.online/api2/',
+     httpEndpoint: 'https://atfrg.space/database/',
     // optional
     // override HTTP endpoint in browser only
     // browserHttpEndpoint: '/graphql',
@@ -12,14 +12,14 @@ export default function(context){
     },
     // You can use `wss` for secure connection (recommended in production)
     // Use `null` to disable subscriptions
-    // wsEndpoint: 'ws://api2/', // optional
+     wsEndpoint: 'wss://atfrg.space/database/', // optional
     // LocalStorage token
     tokenName: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU', // optional
     // Enable Automatic Query persisting with Apollo Engine
-    persisting: true, // Optional
+    persisting: false, // Optional
     // Use websockets for everything (no HTTP)
     // You need to pass a `wsEndpoint` for this to work
-    // websocketsOnly: true, // Optional,
+     websocketsOnly: true, // Optional,
     getAuth: (tokenName) => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU',
   }
 }
