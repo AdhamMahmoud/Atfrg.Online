7c907380dfd11a07082c1babe4962be52c346ffc
Shady Khalifa <shekohex@gmail.com>
2020-05-02 01:47:04 +0200

    rollback

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,19 +1,15 @@
 export default function(context) {
   return {
-    httpEndpoint: "https://api.atfrg.online",
+    httpEndpoint: "https://atfrg.space/database/",
     // ssr: true,
     // optional
     // override HTTP endpoint in browser only
     // browserHttpEndpoint: '/graphql',
     // optional
     // See https://www.apollographql.com/docs/link/links/http.html#options
-    httpLinkOptions: {
-      uri: "/",
-      credentials: "same-origin",
-      fetchOptions: {
-        mode: "no-cors"
-      }
-    },
+    // httpLinkOptions: {
+    //   credentials: 'same-origin'
+    // },
     // You can use `wss` for secure connection (recommended in production)
     // Use `null` to disable subscriptions
     //  wsEndpoint: 'wss://atfrg.space/database/',
@@ -25,6 +21,7 @@ export default function(context) {
     // Use websockets for everything (no HTTP)
     // You need to pass a `wsEndpoint` for this to work
     // websocketsOnly: false, // Optional,
-    getAuth: () => ""
+    getAuth: tokenName =>
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU"
   };
 }
