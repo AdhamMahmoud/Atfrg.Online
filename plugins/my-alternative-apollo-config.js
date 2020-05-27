8d39b05ee87445195771502c9c923d8a0aec2f91
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-05-27 05:18:25 +0200

    back

--- plugins/my-alternative-apollo-config.js
+++ plugins/my-alternative-apollo-config.js
@@ -1,7 +1,7 @@
 export default function(context) {
   return {
     // httpEndpoint: "https://api.atfrg.online/",
-    httpEndpoint: "https://api.atfrg.online/",
+    httpEndpoint: "https://atfrg.space/database/",
     // ssr: true,
     // optional
     // override HTTP endpoint in browser only
@@ -23,6 +23,6 @@ export default function(context) {
     // You need to pass a `wsEndpoint` for this to work
     // websocketsOnly: false, // Optional,
     getAuth: tokenName =>
-      ""
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTg2OTYwODgwLCJleHAiOjE2MTg3ODkyMjN9.2I4N-QE3I2-BgvZr0ilGmictto5q2vNkjnFRGIIKupU"
   };
 }
