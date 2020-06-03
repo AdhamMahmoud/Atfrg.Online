f51b976521d05199d4a26ecda765ceb4b117b209
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-06-03 02:55:24 +0200

    update ad

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,11 +84,11 @@ export default {
       //   src:"https://iclickcdn.com/tag.min.js",
       //   type: "text/javascript"
       // },
-      {
-        src: "//pl15514623.passtechusa.com/84/2a/0b/842a0b5399a51ef446dfd48a4a378db4.js",
-        type: "text/javascript",
-        // defer: true
-      },
+      // {
+      //   src: "//pl15514623.passtechusa.com/84/2a/0b/842a0b5399a51ef446dfd48a4a378db4.js",
+      //   type: "text/javascript",
+      //   // defer: true
+      // },
       
     ]
   },
