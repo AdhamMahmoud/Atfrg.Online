f6e658fd548d19e1a2baef349a1bb8ca9fc47699
Adham Mahmoud <34416159+AdhamMahmoud@users.noreply.github.com>
2020-06-23 20:03:49 +0200

    new

--- layouts/default.vue
+++ layouts/default.vue
@@ -44,15 +44,15 @@ export default {
         }
     },
     mounted() { 
-            var curUrl = window.location.href;
-                if(!curUrl.includes("ar.atfrg.online")){
-                    window.location.href = "https://ar.atfrg.online" + window.location.pathname;
-                }
+            // var curUrl = window.location.href;
+            //     if(!curUrl.includes("ar.atfrg.online")){
+            //         window.location.href = "https://ar.atfrg.online" + window.location.pathname;
+            //     }
             this.timer = setTimeout(() => {
-            // var newScript = document.createElement('script');
-            // newScript.type = 'text/javascript';
-            // newScript.src = '//pl15514623.passtechusa.com/84/2a/0b/842a0b5399a51ef446dfd48a4a378db4.js';
-            // document.getElementsByTagName("head")[0].appendChild(newScript);
+            var newScript = document.createElement('script');
+            newScript.type = 'text/javascript';
+            newScript.src = '//pl15514623.passtechusa.com/84/2a/0b/842a0b5399a51ef446dfd48a4a378db4.js';
+            document.getElementsByTagName("head")[0].appendChild(newScript);
 
             var newScript2 = document.createElement('script');
             newScript2.type = 'text/javascript';
